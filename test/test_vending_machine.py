05453dfc043b70838bbf513d7d57d99619dbc714
logan-j <logan.e.jewett@gmail.com>
2016-10-20 16:09:33 -0700

    Refactor vending machine initialization into test fixture

--- test/test_vending_machine.py
+++ test/test_vending_machine.py
@@ -1,10 +1,15 @@
 import pytest
 from vending_machine.vending_machine import VendingMachine
 
-def test_vending_machine_accept_coin_nickle_returns_true():
+@pytest.fixture
+def vending_machine(scope='session'):
     vending_machine = VendingMachine()
-    assert vending_machine.accept_coin("nickle") == True
+    return vending_machine
 
-def test_vending_machine_accept_coin_coin_returns_false():
-    vending_machine = VendingMachine()
+@pytest.mark.usefixtures('vending_machine')
+def test_vending_machine_accept_coin_nickle_returns_true(vending_machine):
+        assert vending_machine.accept_coin("nickle") == True
+
+@pytest.mark.usefixtures('vending_machine')
+def test_vending_machine_accept_coin_coin_returns_false(vending_machine):
     assert vending_machine.accept_coin("coin") == False
