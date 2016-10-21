810c6ff4bc81b308f252a956d261e7a1d68adcdf
logan-j <logan.e.jewett@gmail.com>
2016-10-20 23:09:28 -0700

    Added a purchase menu and other base methods

--- test/test_vending_machine.py
+++ test/test_vending_machine.py
@@ -1,22 +1,23 @@
 import pytest
 from vending_machine.vending_machine import VendingMachine
 
-@pytest.fixture
-def vending_machine(scope='session'):
+# Function scoped to be more explicit with test state
+@pytest.fixture(scope='function')
+def vending_machine():
     vending_machine = VendingMachine()
     return vending_machine
 
 @pytest.mark.usefixtures('vending_machine')
 def test_vending_machine_accept_coin_nickle_returns_true(vending_machine):
-        assert vending_machine.accept_coin("nickle") == True
+        assert vending_machine.accept_coin('nickle') == True
 
 @pytest.mark.usefixtures('vending_machine')
 def test_vending_machine_accept_coin_coin_returns_false(vending_machine):
-    assert vending_machine.accept_coin("coin") == False
+    assert vending_machine.accept_coin('coin') == False
 
 @pytest.mark.usefixtures('vending_machine')
 def test_vending_machine_accept_coin_nickle_adds_value(vending_machine):
-    vending_machine.accept_coin("nickle")
+    vending_machine.accept_coin('nickle')
     assert vending_machine.inserted_value() == 5
 
 @pytest.mark.usefixtures('vending_machine')
@@ -25,3 +26,13 @@ def test_vending_machine_return_coins(vending_machine):
     coins = vending_machine.return_coins()
     assert coins == ['nickle']
     assert vending_machine.inserted_value() == 0
+
+@pytest.mark.usefixtures('vending_machine')
+def test_vending_machine_get_purchase_menu(vending_machine):
+    menu = vending_machine.get_purchase_menu()
+    assert menu == {'cola': 0, 'chips': 1, 'candy': 2}
+
+@pytest.mark.usefixtures('vending_machine')
+def test_vending_machine_purchase_chips_returns_nothing(vending_machine):
+    button_to_press = vending_machine.get_purchase_menu()['chips']
+    assert vending_machine.purchase(button_to_press) == []
--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -1,35 +1,44 @@
-"""
+'''
 @author: Logan Jewett
 
-"""
+'''
 
 from collections import defaultdict
 
 class VendingMachine:
 
     default_coins = {
-        "nickle": 5,
-        "dime": 10,
-        "quarter": 25
+        'nickle': 5,
+        'dime': 10,
+        'quarter': 25
     }
 
-    """
+    default_products = [
+        {'name': 'cola', 'cost': 100},
+        {'name': 'chips', 'cost': 50},
+        {'name': 'candy', 'cost': 65}
+    ]
+
+    '''
     Initialize this vending machine
 
     @param coins: coins is a dictionary input of string to integer mappings of
                   coins this vending machine will accept by name and their
                   associated values in cents.
-    """
-    def __init__(self, coins=default_coins):
+    '''
+    def __init__(self, coins=default_coins, products=default_products):
         self.coins_accepted = coins
+        self.products = products
+
         self.inserted_coins = []
         self.register = defaultdict(int)
+        self.inventory = defaultdict(int)
 
-    """
+    '''
     Attempt to insert a coin of NAME into the vending machine
     @param coin: coin is a string input representation of a coin that's being
                  inserted into the vending machine
-    """
+    '''
     def accept_coin(self, coin):
         if coin in self.coins_accepted.keys():
             self.inserted_coins.append(coin)
@@ -37,10 +46,28 @@ class VendingMachine:
         else:
             return False
 
+    '''
+    @return: The total value of inserted coins in cents
+    '''
     def inserted_value(self):
         return sum(self.coins_accepted[coin] for coin in self.inserted_coins)
 
+    '''
+    @return: A list of all coins that have been inserted so far
+    '''
     def return_coins(self):
         coins_out = self.inserted_coins
         self.inserted_coins = []
         return coins_out
+
+    '''
+    @return: A dictionary mapping item name to button number
+    '''
+    def get_purchase_menu(self):
+        return dict(zip(
+            [product['name'] for product in self.products],
+            xrange(0, len(self.products))
+        ))
+
+    def purchase(self, button_number):
+        return []
