8b2e4e0a5ebdfd181a0bd6ed27f104f12260ec58
logan-j <logan.e.jewett@gmail.com>
2016-10-25 22:49:15 -0700

    Some more refactoring and documentation; tweaked _stock_vending_machine to handle new products; changed get menu to get product button -> use inventory for menu

--- test/test_vending_machine.py
+++ test/test_vending_machine.py
@@ -42,12 +42,8 @@ def test_vending_machine_return_coins(vending_machine):
     assert coins == ['nickle']
     assert vending_machine._inserted_value() == 0
 
-def test_vending_machine_get_purchase_menu(vending_machine):
-    menu = vending_machine.get_purchase_menu()
-    assert menu == {'cola': 0, 'chips': 1, 'candy': 2}
-
 def test_vending_machine_purchase_chips_returns_nothing(vending_machine):
-    button_to_press = vending_machine.get_purchase_menu()['chips']
+    button_to_press = vending_machine.get_product_button('chips')
     chips_question_mark = vending_machine.select_product(button_to_press)
     assert chips_question_mark == []
 
@@ -55,7 +51,7 @@ def test_vending_machine_default_display(vending_machine):
     assert vending_machine.show_display() == 'INSERT COIN'
 
 def test_vending_machine_display_purchase_chips_shows_price_then_insert_coin(vending_machine):
-    button_to_press = vending_machine.get_purchase_menu()['chips']
+    button_to_press = vending_machine.get_product_button('chips')
     chips_question_mark = vending_machine.select_product(button_to_press)
     assert vending_machine.show_display() == 'PRICE $0.50'
     assert vending_machine.show_display() == 'INSERT COIN'
@@ -63,7 +59,7 @@ def test_vending_machine_display_purchase_chips_shows_price_then_insert_coin(ven
 def test_vending_machine_purchase_chips_returns_sold_out(vending_machine):
     vending_machine.accept_coin('quarter')
     vending_machine.accept_coin('quarter')
-    button_to_press = vending_machine.get_purchase_menu()['chips']
+    button_to_press = vending_machine.get_product_button('chips')
     chips_question_mark = vending_machine.select_product(button_to_press)
     assert chips_question_mark == []
     assert vending_machine.show_display() == 'SOLD OUT'
@@ -79,7 +75,7 @@ def test_vending_machine_reject_coin_goes_to_coin_return(vending_machine):
 def test_vending_machine_sold_out_display_goes_to_inserted_value(vending_machine):
     vending_machine.accept_coin('quarter')
     vending_machine.accept_coin('quarter')
-    button_to_press = vending_machine.get_purchase_menu()['chips']
+    button_to_press = vending_machine.get_product_button('chips')
     vending_machine.select_product(button_to_press)
     vending_machine.show_display()
     assert vending_machine.show_display() == '$0.50'
@@ -87,7 +83,7 @@ def test_vending_machine_sold_out_display_goes_to_inserted_value(vending_machine
 def test_vending_machine_sold_out_display_no_money_goes_to_insert_coin(vending_machine):
     vending_machine.accept_coin('quarter')
     vending_machine.accept_coin('quarter')
-    button_to_press = vending_machine.get_purchase_menu()['chips']
+    button_to_press = vending_machine.get_product_button('chips')
     vending_machine.select_product(button_to_press)
     vending_machine.return_coins()
     vending_machine.show_display()
@@ -100,14 +96,14 @@ def test_vending_machine_return_coins_sets_display_to_insert_coin(vending_machin
     assert vending_machine.show_display() == 'INSERT COIN'
 
 def test_stock_vending_machine_adds_products(vending_machine):
-    slot_number = vending_machine.get_purchase_menu()['chips']
+    slot_number = vending_machine.get_product_button('chips')
     vending_machine._stock_vending_machine(slot_number, 1)
     assert vending_machine.inventory[slot_number]['product_count'] == 1
 
 def test_vending_machine_purchase_product_display_says_thank_you_then_reset(vending_machine):
     vending_machine.accept_coin('quarter')
     vending_machine.accept_coin('quarter')
-    button_to_press = vending_machine.get_purchase_menu()['chips']
+    button_to_press = vending_machine.get_product_button('chips')
     vending_machine._stock_vending_machine(button_to_press, 1)
     vending_machine.select_product(button_to_press)
     assert vending_machine.show_display() == 'THANK YOU'
@@ -116,7 +112,7 @@ def test_vending_machine_purchase_product_display_says_thank_you_then_reset(vend
 def test_vending_machine_returns_product_and_decreases_inventory(vending_machine):
     vending_machine.accept_coin('quarter')
     vending_machine.accept_coin('quarter')
-    button_to_press = vending_machine.get_purchase_menu()['chips']
+    button_to_press = vending_machine.get_product_button('chips')
     vending_machine._stock_vending_machine(button_to_press, 1)
     chips_question_mark = vending_machine.select_product(button_to_press)
     assert chips_question_mark == ['chips']
@@ -126,9 +122,45 @@ def test_vending_machine_purchase_product_returns_change(vending_machine):
     vending_machine.accept_coin('quarter')
     vending_machine.accept_coin('quarter')
     vending_machine.accept_coin('quarter')
-    button_to_press = vending_machine.get_purchase_menu()['candy']
+    button_to_press = vending_machine.get_product_button('candy')
     vending_machine._stock_vending_machine(button_to_press, 1)
     vending_machine._refill_coin_register('dime', 100)
     vending_machine.select_product(button_to_press)
-    print vending_machine.coin_return
     assert vending_machine.empty_coin_return() == ['dime']
+
+def test_vending_machine_get_product_button_no_product_returns_neg_one(vending_machine):
+    button_to_press = vending_machine.get_product_button('cheese')
+    assert button_to_press == -1
+
+def test_vending_machine_get_product_button_returns_candy_position(vending_machine):
+    button_to_press = vending_machine.get_product_button('candy')
+
+    # 0: cola
+    # 1: chips
+    # 2: candy
+    assert button_to_press == 2
+
+def test_vending_machine_stock_new_product(vending_machine):
+    vending_machine._stock_vending_machine(
+        4,
+        {'product_name': 'cheese', 'product_cost': 150, 'product_count': 1}
+    )
+
+    button_to_press = vending_machine.get_product_button('cheese')
+    assert button_to_press == 4
+
+def test_vending_machine_get_product_button_returns_candy_slot(vending_machine):
+
+    vending_machine._stock_vending_machine(
+        4,
+        {'product_name': 'candy', 'product_cost': 65, 'product_count': 1}
+    )
+
+    # 0: cola
+    # 1: chips
+    # 2: candy <- this candy slot is empty
+    # 3: nothing
+    # 4: candy <- return this
+
+    candy_location = vending_machine.get_product_button('candy')
+    assert candy_location == 4
--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -2,7 +2,6 @@
 @author: Logan Jewett
 
 '''
-import sys
 from collections import defaultdict
 
 class VendingMachine:
@@ -35,20 +34,24 @@ class VendingMachine:
         for coin in coins:
             self._add_accepted_coin(coin)
 
-        self.inventory = products
-        for product in self.inventory:
+        self.inventory = defaultdict(dict)
+        for index, product in enumerate(products):
+            self.inventory[index] = product
+        for product in self.inventory.values():
             if not product.has_key('product_count'):
                 product['product_count'] = 0
 
         self.display = 'INSERT COIN' # Physical Display
-        self.inserted_coins = list() # P
+        self.inserted_coins = list()
 
         self.coin_return = list() # Physical Coin Return
 
     '''
-    Attempt to insert a coin of COIN_NAME into the vending machine
-    @param coin: coin is a string input representation of a coin that's being
-                 inserted into the vending machine
+    Attempt to insert a coin of COIN_NAME into the vending machine.
+    @param coin_name: coin_name is a string input representation of a coin that's being
+                      inserted into the vending machine.
+
+    @return: True if a valid coin, else False
     '''
     def accept_coin(self, coin_name):
         if self._check_coin_accepted(coin_name):
@@ -60,33 +63,51 @@ class VendingMachine:
             self.display = 'INSERT COIN'
             return False
 
-    '''
-    @return: The total value of inserted coins in cents
-    '''
+
     def _inserted_value(self):
         return sum(self._coin_name_to_value_map[coin] for coin in self.inserted_coins)
 
-    def _stock_vending_machine(self, slot_number, product_count):
-        self.inventory[slot_number]['product_count'] = product_count
+
+    def _stock_vending_machine(self, slot_number, product_or_count):
+        if type(product_or_count) == int:
+            self.inventory[slot_number]['product_count'] = product_or_count
+        elif type(product_or_count) == dict:
+            self.inventory[slot_number] = product_or_count
+
 
     def _refill_coin_register(self, coin_name, coin_count):
         self._register[coin_name] += coin_count
 
+    '''
+    Reverse lookup for a coin_name given a specific value. Helper method for simplicity's
+    sake during the process of making change.
+    @param coin_value: Value of the coin_name wanted given its value in cents
+
+    @return: coin_name desired for value, else None if not found
+    '''
     def _get_coin_name(self, coin_value):
         for name, value in dict.iteritems(self._coin_name_to_value_map):
             if coin_value == value:
                 return name
         return None
 
+
     def _check_coin_accepted(self, coin_name):
         return self._coin_name_to_value_map.has_key(coin_name)
 
+    '''
+    Helper method for intialization to check if a coin of given name or value is
+    already represented, then stores that information if not.
+    @param coin: Dictionary of information for a given coin
+                 e.g. {'coin_name': 'quarter', 'coin_value': 25, 'coin_count': 10}
+    '''
     def _add_accepted_coin(self, coin):
         if not self._check_coin_accepted(coin['coin_name']) and\
                 self._get_coin_name(coin['coin_value']) == None:
 
             self._coin_name_to_value_map[coin['coin_name']] = coin['coin_value']
             self._register[coin['coin_name']] = coin.get('coin_count', 0)
+
     # def _coins_available(self):
     #     coins_available = []
     #     for coin_name, coin_count in dict.iteritems(self._register):
@@ -94,26 +115,33 @@ class VendingMachine:
     #             coins_available.append(self._coin_name_to_value_map[coin_name])
     #     return coins_available
 
+    '''
+    Adaptation of a well known iterative algorithm for making change
+    @param change_to_make: value in cents of how much change to make
+
+    @return: a list of coin_names to return in order to meet the needed amount of change
+    '''
     def _make_change(self, change_to_make):
-        minCoins = [0] * (change_to_make + 1)
-        coinsUsed = [0] * (change_to_make + 1)
+        min_coins = [0] * (change_to_make + 1)
+        coins_used = [0] * (change_to_make + 1)
         for cents in range(change_to_make + 1):
 
-            coinCount = cents
-            newCoin = 1
-            for j in [c for c in self._coin_name_to_value_map.values() if c <= cents]:
-                if minCoins[cents-j] + 1 < coinCount:
-                    coinCount = minCoins[cents-j]+1
-                    newCoin = j
-            minCoins[cents] = coinCount
-            coinsUsed[cents] = newCoin
+            coin_count = cents
+            new_coin = 1
+            for coin in [c for c in self._coin_name_to_value_map.values() if c <= cents]:
+                if min_coins[cents-coin] + 1 < coin_count:
+                    coin_count = min_coins[cents-coin]+1
+                    new_coin = coin
+            min_coins[cents] = coin_count
+            coins_used[cents] = new_coin
 
-        coins_returned = []
+        coins_returned = list()
         change_left_to_make = change_to_make
+        coins_available = self._register
         while change_left_to_make > 0:
-            thisCoin = coinsUsed[change_left_to_make]
-            change_left_to_make -= thisCoin
-            coins_returned.append(self._get_coin_name(thisCoin))
+            this_coin = coins_used[change_left_to_make]
+            change_left_to_make -= this_coin
+            coins_returned.append(self._get_coin_name(this_coin))
         return coins_returned
 
 
@@ -128,19 +156,27 @@ class VendingMachine:
         self.coin_return = []
         return coins_out
 
+    '''
+    @return: Button/slot_number of a given product_name, -1 if no slots of given
+             product_name
+    '''
+    def get_product_button(self, product_name):
+        return_index = -1
+
+        for index, product in dict.iteritems(self.inventory):
+            if product['product_name'] == product_name:
+                return_index = index
+                if product['product_count'] > 0:
+                    break
+
+        return return_index
 
     '''
-    @return: A dictionary mapping item name to button number
+    The action of making a selection on the Vending Machine
     '''
-    def get_purchase_menu(self):
-        return dict(zip(
-            [product['product_name'] for product in self.inventory],
-            xrange(0, len(self.inventory))
-        ))
-
-
     def select_product(self, button_number):
         product = self.inventory[button_number]
+        print self.inventory
         inserted_value = self._inserted_value()
         if inserted_value < product['product_cost']:
             self.display = 'PRICE $%.2f' % (product['product_cost'] / 100.0)
@@ -153,7 +189,7 @@ class VendingMachine:
             self.coin_return = change
             self.inserted_value = []
             return [product['product_name']]
-        return []
+        return list()
 
 
     def show_display(self):
